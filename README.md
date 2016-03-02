269accca45a8e12c652065b9d340b9f1a40f351c
Daniel Robinson <d.robinson004@gmail.com>
2016-03-02 14:04:30 -0500

    new logo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Siderman Vinyl
+
+## Deploying
+There are two environments: production, and staging.
+To deploy to the either environment run the following:
+`
+rake deploy:<environment>
+`
--- config.rb
+++ config.rb
@@ -1,3 +1,8 @@
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :relative_links, true
+
 ###
 # Compass
 ###
@@ -11,7 +16,7 @@
 # Page options, layouts, aliases and proxies
 ###
 
-activate :directory_indexes
+# activate :directory_indexes
 
 # Per-page layout changes:
 #
@@ -37,11 +42,6 @@ activate :directory_indexes
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
 # Methods defined in the helpers block are available in templates
 helpers do
   def nav_item(text, url, options={})
@@ -51,31 +51,46 @@ helpers do
   end
 
   def nav_class_if_active(url)
-    url = "#{url}/" unless /\/$/ =~ url
-    'active' if url == current_page.url
+    url = url.sub(/index\.html$/, '')
+    # url = "#{url}/" unless /\/$/ =~ url
+    'active' if url == current_resource.url
+  end
+
+  def nav_classes_for(url)
+    classes = []
+    path = url.sub(/index\.html$/, '')
+    current_url = current_resource.url
+    classes << "on" if current_url == path
+    classes << "in" if current_url.include?(path)
+    classes.join(' ')
   end
 end
 
-set :css_dir, 'stylesheets'
+case ENV['TARGET'].to_s.downcase
+when 'staging'
+  activate :deploy do |deploy|
+    deploy.method   = :git
+    deploy.build_before = true
+    # set :http_prefix, '/siderman'
+  end
+else
+  activate :deploy do |deploy|
 
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+  end
+end
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :relative_assets
+  set :relative_links, true
 end
